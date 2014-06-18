b7b07d7b9daebf43e7c2eb6b6971ba9a3ff1052a
Eron Hennessey <eron@abstrys.com>
2014-06-18 03:26:44 -0700

    Fixes for the setup files.

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 from distutils.core import setup
 
-long_desc = open('README.rst').read()
+long_desc = open('README.txt').read()
 
 setup(name='rst2db',
       description="""
         A reStructuredText to DocBook converter using Python's docutils.""",
-      version='0.9',
+      version='0.9b',
       requires=['docutils'],
       packages=['abstrys'],
       scripts=['rst2db.py'],
